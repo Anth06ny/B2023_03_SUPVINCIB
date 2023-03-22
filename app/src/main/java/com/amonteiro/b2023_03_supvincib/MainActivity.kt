6964384c32b04fb6527ab9597056f49b03c1cbfd
Anthony <anth06ny@gmail.com>
2023-03-22 13:36:56 +0100

    Correction Manip IHM Partie 1

--- app/src/main/java/com/amonteiro/b2023_03_supvincib/MainActivity.kt
+++ app/src/main/java/com/amonteiro/b2023_03_supvincib/MainActivity.kt
@@ -2,15 +2,28 @@ package com.amonteiro.b2023_03_supvincib
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.amonteiro.b2023_03_supvincib.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    //creation des composants graphiques
+    //lazy action a retardement à la 1ere utilisation
+    val binding by lazy {   ActivityMainBinding.inflate(layoutInflater)}
 
     //Création de l'écran
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //Affiche une IHM
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btValidate.setOnClickListener {
+            binding.et.setText("Clic sur valider")
+        }
+
+        binding.btCancel.setOnClickListener {
+            binding.et.setText("Clic sur annuler")
+        }
 
     }
 }
