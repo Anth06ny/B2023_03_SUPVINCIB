9fd604f81c18831179d0bf806725c3d72288ac2f
Anthony <anth06ny@gmail.com>
2023-03-22 17:03:08 +0100

    Correction TP Changement d'écran

--- app/src/main/java/com/amonteiro/b2023_03_supvincib/WeatherActivity.kt
+++ app/src/main/java/com/amonteiro/b2023_03_supvincib/WeatherActivity.kt
@@ -2,13 +2,32 @@ package com.amonteiro.b2023_03_supvincib
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.amonteiro.b2023_03_supvincib.databinding.ActivityWeatherBinding
+import kotlin.concurrent.thread
 
 class WeatherActivity : AppCompatActivity() {
+
+    val binding by lazy {   ActivityWeatherBinding.inflate(layoutInflater)}
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_weather)
+        setContentView(binding.root)
 
-        println("WeatherActivity.onCreate")
+        binding.btLoad.setOnClickListener {
+
+            binding.tv.setText("Avant Thread")
+            thread {
+                var data = RequestUtils.loadWeather("Nice")
+                Thread.sleep(5000)
+
+                //Permet de revenir sur le thread Principale
+                runOnUiThread {
+                    binding.tv.setText("Il fait ${data.main.temp}° à ${data.name} avec un vent de ${data.wind.speed} km/h")
+                }
+            }
+        }
+
     }
 
 }
